9d69c2c19668081038fc6c0a4d3bbb2de7557aa6
Ricardo Lobo <ricardolobo@audienciazero.org>
2015-10-21 18:04:12 +0100

    changes default value on items serializer for display fields on related models

--- boxing/api/serializers.py
+++ boxing/api/serializers.py
@@ -12,6 +12,15 @@ class ContainerSerializer(serializers.HyperlinkedModelSerializer):
                 fields = ('url', 'id', 'name', 'created', 'updated')
 
 class ItemSerializer(serializers.HyperlinkedModelSerializer):
+        # display field for the browsable api, drf by default
+        # uses __str__ method of model
+        class RelatedModelDisplayField(serializers.PrimaryKeyRelatedField):
+                def display_value(self, instance):
+                        return instance.name
+                
+        category = RelatedModelDisplayField(queryset=Category.objects.all())
+        container = RelatedModelDisplayField(queryset=Container.objects.all())
+        
         class Meta:
                 model = Item
                 fields = ('url', 'id', 'name', 'image', 'container', 'category', 'quantity', 'created', 'updated')
