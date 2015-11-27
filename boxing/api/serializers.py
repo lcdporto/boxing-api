ce3f1f0401b01e95eb27628f4d4a9414e835a670
Ricardo Lobo <ricardolobo@audienciazero.org>
2015-11-27 22:16:08 +0000

    removes hyperlinked api

--- boxing/api/serializers.py
+++ boxing/api/serializers.py
@@ -1,17 +1,17 @@
 from rest_framework import serializers
 from boxing.api.models import *
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
         class Meta:
                 model = Category
-                fields = ('id', 'url', 'name', 'created', 'updated')
+                fields = ('id', 'name', 'created', 'updated')
 
-class ContainerSerializer(serializers.HyperlinkedModelSerializer):
+class ContainerSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Container
-                fields = ('url', 'id', 'name', 'created', 'updated')
+                fields = ('id', 'name', 'created', 'updated')
 
-class ItemSerializer(serializers.HyperlinkedModelSerializer):
+class ItemSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Item
-                fields = ('url', 'id', 'name', 'image', 'container', 'category', 'quantity', 'created', 'updated')
+                fields = ('id', 'name', 'image', 'container', 'category', 'quantity', 'created', 'updated')
