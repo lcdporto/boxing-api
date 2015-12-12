c6d1228239f713d0ea199445836839fdfb1aedaa
Ricardo Lobo <ricardolobo@audienciazero.org>
2015-12-12 22:22:47 +0000

    renames build folder to dist; changes dockerfile and initialize to match that change; updates initilize to copy a dev database

--- initialize.sh
+++ initialize.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# presently we only have set the dev build
+# log this build
+echo $(date +"%d_%m_%Y_%H_%M_%S") >> logs/initialize.log
+
+# settings variables to use in this script
+# only using development build for now
 BUILD=development
 PROJECT=boxing
 PROJECT_ROOT=/var/www/boxing-api
-
-# log this build
-echo $(date +"%d_%m_%Y_%H_%M_%S") >> logs/initialize.log
 
 # delete default nginx config
 rm /etc/nginx/sites-enabled/default
 
 # set ngninx configurations
-cp build/nginx/$BUILD/$PROJECT /etc/nginx/sites-available/
+cp dist/$BUILD/nginx/$PROJECT /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/$PROJECT /etc/nginx/sites-enabled/$PROJECT
 
 # set settings file
-ln -s $PROJECT_ROOT/build/django/$BUILD/settings.py $PROJECT_ROOT/boxing/settings.py
+ln -s $PROJECT_ROOT/dist/$BUILD/django/settings.py $PROJECT_ROOT/boxing/settings.py
 
-# initialize Django project
+# collect static files (e.g. images to the static folder as defined in the settings)
 python3 $PROJECT_ROOT/manage.py collectstatic --noinput
-python3 $PROJECT_ROOT/manage.py makemigrations --noinput
-python3 $PROJECT_ROOT/manage.py migrate --noinput
 
-# create superuser
+# copy the development database if not already present
+if [ "$APP_IN_PRODUCTION" = false ]; then
+    echo "we are in development mode, a database will be created if not already present..."
+    # the -n flag means no clobber, i.e do not replace an existing file
+    cp -n dist/$BUILD/database/boxing.sqlite3 $PROJECT_ROOT/
