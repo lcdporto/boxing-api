cfaffb5d32b02d08d4c024d2af6fd2167f65cce4
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-07-22 23:14:06 +0100

    adds set undefined quantity admin action

--- boxing/api/admin.py
+++ boxing/api/admin.py
@@ -2,10 +2,16 @@ from django.contrib import admin
 
 from boxing.api import models
 
+
+def set_undefined_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=None)
+set_undefined_quantity.short_description = "Set Items Quantity to Undefined"
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'container', 'category', 'quantity')
     list_filter = ('container', 'category', 'quantity')
     search_fields = ('name', )
+    actions = [set_undefined_quantity]
 
 admin.site.register(models.Item, ItemAdmin)
 admin.site.register(models.Container)
