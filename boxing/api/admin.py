60f2e0d0cc4993ad4e9344ff6e06f3015fc28b58
Ricardo Lobo <ricardolobo@audienciazero.org>
2016-07-23 11:45:03 +0100

    makes set undefined quantity a method and adds user feedback

--- boxing/api/admin.py
+++ boxing/api/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 
 from boxing.api import models
 
-
-def set_undefined_quantity(modeladmin, request, queryset):
-    queryset.update(quantity=None)
-set_undefined_quantity.short_description = "Set Items Quantity to Undefined"
-
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'container', 'category', 'quantity')
     list_filter = ('container', 'category', 'quantity')
     search_fields = ('name', )
-    actions = [set_undefined_quantity]
+    actions = ['set_undefined_quantity']
+
+    def set_undefined_quantity(self, request, queryset):
+        updated = queryset.update(quantity=None)
+        self.message_user(request, "successfully set {0} items quantity to undefined.".format(updated))
+    set_undefined_quantity.short_description = "Set Items Quantity to Undefined"
 
 admin.site.register(models.Item, ItemAdmin)
 admin.site.register(models.Container)
